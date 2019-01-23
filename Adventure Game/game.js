998483aa4e1f135b35e4fa10d343bda993ac14b2
Melvin Whitaker <awsome7mailman@gmail.com>
2019-01-23 10:27:41 -0700

    Introduction to javascript

---
+++ Adventure Game/game.js
@@ -0,0 +1,15 @@
+// Single line comment
+/*
+Multiline comment
+*/
+
+// document.write("I can't wait for <em>joker</em> for Smash DLC!");
+
+// alert("Warning! Will Robinson...Warning!");
+
+// confirm("Do you like pokemon?");
+
+// prompt("What type of pokemon do you like?");
+
+var pc = prompt("What is your name?");
+confirm("So your name is " + pc);
